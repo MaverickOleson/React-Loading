0a76a7e22d632ae293d08f849f8fdd956a88aac1
Maverick Oleson <Moleso587@west-mec.local>
2021-09-13 15:21:35 -0700

    gallery page, nav, images, changed load styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './styles/App.css';
+import './styles/pyramid.css';
 import React, {useState, useEffect} from 'react';
 import Loading from './loading';
 
 function App() {
   const [IsLoading, setIsLoading] = useState(true);
   const [Error, setError] = useState(false);
+  const [imgRotate, setImgRotate] = useState(0);
+  const imgSrcs = [
+    'https://upload.wikimedia.org/wikipedia/commons/5/57/Bob_Moog3.jpg',
+  ]
   useEffect(() => {
     try {
       setTimeout(() => {setIsLoading(false)}, 3400);
@@ -21,7 +26,18 @@ function App() {
   else{
     return (
       <div className="App">
-        <h1>sf</h1>
+        <nav>
+          <h1>Home</h1>
+          <h1>Gallery</h1>
+          <h1>Biography</h1>
+        </nav>
+        <main>
+          <h1>Gallery</h1>
+          <div className="galleryBox">
+            <img src={imgSrcs[imgRotate]} alt="" />
+            <img src="" alt="" />
+          </div>
+        </main>
       </div>
     );
   }
